3761c0a3e05f2e69fcd3a372e51e1a8a7846d999
André Souza <56655392+Drenossauro@users.noreply.github.com>
2024-12-30 00:25:06 -0400

    Add img

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home(){
     return <div>
         <h1>Karim!!!</h1>
         <p>Rafinha, eu te amo, se voce me ama da um sorrisinho</p>
+        <img src="https://imgur.com/a/N7yW3Sk"/>
     </div>
 }
 
