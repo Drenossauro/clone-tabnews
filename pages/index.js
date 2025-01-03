84dbdd6142045e4c49317ed6f76adb9e9bb3160c
André Souza <56655392+Drenossauro@users.noreply.github.com>
2025-01-02 21:56:34 -0400

    Remoção da imagem

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home(){
     return <div>
-        <h1>Karim!!!</h1>
+        <h1>Oie!!!</h1>
         <p>Rafinha, eu te amo, se voce me ama da um sorrisinho</p>
-        <img src="https://imgur.com/DHDXSZw"/>
     </div>
 }
 
