898876593190a596ee068936edec2c5a2c3aab00
André Souza <56655392+Drenossauro@users.noreply.github.com>
2025-01-19 14:06:59 -0400

    Install prettier

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-function Home(){
-    return <div>
-        <h1>Oie!!!</h1>
-        <p>Rafinha, eu te amo, se voce me ama da um sorrisinho</p>
+function Home() {
+  return (
+    <div>
+      <h1>Oie!!!</h1>
+      <p>Rafinha, eu te amo, se voce me ama da um sorrisinho</p>
     </div>
+  );
 }
-
 export default Home;
